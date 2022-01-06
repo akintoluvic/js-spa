fc15c95cc6ebf9a6720484b85c284b9119034eb1
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-01-06 11:16:51 +0100

    server running

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const app = express();
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", 'index.html'));
 })
+
+app.listen(3800, () => {
+    console.log("Server started on port 3800");
+})
