f79d28b935524092cc9465e858a7855d6a7d2026
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-01-06 11:30:08 +0100

    router init and potental matches

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,19 @@
-console.log('JS loaeded');
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("View dashboard")},
+        { path: "/posts", view: () => console.log("View posts")},
+        { path: "/settings", view: () => console.log("View settings")},
+    ]
+
+    // Test routes for potential matches
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    })
+
+    console.log(potentialMatches);
+}
+
+document.addEventListener("DOMContentLoaded", router);
