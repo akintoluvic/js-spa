21c9a4ecb27b83f061726df4c775e07ff874c597
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-01-08 12:44:37 +0100

    get params

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,6 +2,19 @@ import Dashboard from "./views/Dashboard.js"
 import Posts from "./views/Posts.js"
 import Settings from "./views/Settings.js"
 
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$")
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    console.log(values, keys)
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }))
+}
+
 const navigateTo = url => {
     history.pushState(null, null, url)
     router()
@@ -11,6 +24,7 @@ const router = async () => {
     const routes = [
         { path: "/", view: Dashboard },
         { path: "/posts", view: Posts },
+        { path: "/posts/:id", view: Posts },
         { path: "/settings", view: Settings },
     ]
 
@@ -18,22 +32,23 @@ const router = async () => {
     const potentialMatches = routes.map(route => {
         return {
             route: route,
-            isMatch: location.pathname === route.path
+            result: location.pathname.match(pathToRegex(route.path))
         }
     })
 
     // Find the exact match
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null)
 
     // If there is no exact match, return to the home page
     if(!match) {
         match = {
-            route: routes[0]
+            route: routes[0],
+            result: [location.pathname]
         }
     }
 
     // 
-    const view = new match.route.view()
+    const view = new match.route.view(getParams(match))
 
     document.querySelector("#app").innerHTML = await view.getHtml()
     
